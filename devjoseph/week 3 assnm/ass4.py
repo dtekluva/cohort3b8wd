1cb1fdbaddb456b64426b80d964a781ffd2a10bc
mobolajijohnson <mobolajijohnson13@gmail.com>
2021-02-08 16:20:35 +0100

    clean up

--- devjoseph/week 3 assnm/ass4.py
+++ devjoseph/week 3 assnm/ass4.py
@@ -9,4 +9,73 @@
 
 
 
-first name = input("enter your first name")
+# first name = input("enter your first name")
+# last name = input("enter your last name")
+# file.write(firstname)
+
+
+# age =int(input("what is your age? "))
+# if age > 25:
+#     Answer = ("HAPPY BIRTHDAY TO YOU")
+#     print(Answer)
+# else:
+#     print("YOU ARE NOT DUE FOR CELEBRATION YET")
+
+
+
+# a = int(input("A > "))
+# b = int(input("B > "))
+# m = a if b else b
+# print(m)
+
+# cost_price = int(input( "add the cost price "))
+# selling_price = int(input("add the selling price "))
+# if cost_price > selling_price :
+#     loss = cost_price - selling_price
+#     print("loss")
+#     loss_percent = (loss)/100
+#     print(loss_percent)
+#     profit = selling_price - cost_price
+#     profit_percent = (profit)/100
+#     print(profit)
+    
+
+# if 'bar' in {'foo': 1, 'bar': 2, 'baz': 3}:
+#     print(1)
+#     print(2)
+
+    a = 100
+    b = 50
+    m = a if b > 50 :
+    print(m)
+
+if a < b:
+    m = a
+else:
+    m = b
+
+ 
+
+
+
+    # print("write a program that tells you that a number is a prime number")
+    # Y = int(input("please provide a number"))
+    # if Y =
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
