bc5ac713d415ab5bfbc776646ff6c267b2fc49f5
mobolajijohnson <mobolajijohnson13@gmail.com>
2021-02-08 11:33:04 +0100

    my message

---
+++ devjoseph/week 3 assnm/ass4.py
@@ -0,0 +1,12 @@
+# write a program that takes a name and last name seperated by comma and write the first name into
+# a file named the last name
+
+# firstname = input("enter your first name: ")
+# lastname = input("enter your last name: ")
+
+# file = open(f"./devjoseph/week 3 assnm/{lastname}.txt", "w")
+# file.write(firstname)
+
+
+
+first name = input("enter your first name")
---
+++ johnson/mypythonexcercise.py
@@ -0,0 +1,57 @@
+# Bola = int(input("how many sweet did you bring"))
+# Tola = int(input("how many sweet did you bring"))
+# Obum = int(input("how many sweet did you bring"))
+# print("from first statement")
+# disc = (Bola + Tola + Obum)
+# print(disc)
+# equal_share = disc / 3
+# remainder = disc%3
+# floor = disc // 3
+# bottomeline = ("each friend will share {disc} and get {equal_share} while it will {remain} sweet")
+# print(bottomeline)
+
+
+# x = 31
+# age = int(int("how old are you"))
+# print("age is <6")
+
+
+# x = 10
+# y =20
+# age =int(input("how old are you?"))
+
+# print("from here : {x > y")
+# print("from here : {x < y")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# Bola = int(input("how many sweet did you bring"))
+# Tola = int(input("how many sweet did you bring"))
+# Obum = int(input("how many sweet did you bring"))
+# total = (Bola + Tola + Obum)
+# number_of_friends = 3
+# equal_share = total // number_of_friends
+# excess = total % number_of_friends
+# print(f"Each friend will get {equal_share}sweet and {excess} will be discarded.")
+
+
+
+
+
+
+
+
+
