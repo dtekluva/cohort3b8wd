f4cc0ee20b0d45b28c63b5bb422c217b8d200122
mobolajijohnson <mobolajijohnson13@gmail.com>
2021-02-10 16:15:48 +0100

    my message

---
+++ johnson/assignment.py
@@ -0,0 +1,142 @@
+# How to determine whether a year is a leap year
+# To determine whether a year is a leap year, follow these steps:
+
+# If the year is evenly divisible by 4, go to step 2. Otherwise, go to step 5.
+# If the year is evenly divisible by 100, go to step 3. Otherwise, go to step 4.
+# If the year is evenly divisible by 400, go to step 4. Otherwise, go to step 5.
+# The year is a leap year (it has 366 days).
+# The year is not a leap year (it has 365 days).
+
+
+
+# print("How to determine whether a year isH a leap year :  ")
+# year = int(input("please insert the year"))
+# step1 = year % 4 
+# step2 = year % 100
+# step3 = year % 400
+# if ("step1 is divisible by 4, go to step 2"):
+#     print(step2)
+# else:
+#      print(step3)
+
+
+
+#   n = 5
+# >>> while n > 0:
+#      n -= 1
+#       print(n)
+
+# n = 5
+# while n > 0:
+#     n -= 1
+#     print(n)
+
+
+# n = 5
+# while n > 0:
+#     n -= 1
+#     print(n)
+
+
+# print("write a count down timer that counts to seconds -")
+# import time
+# count_down = int(input("please inser your time - "))
+# while count_down:
+#     count_down -= 1
+#     time.sleep(1)
+#     print(f"{count_down} seconds left")
+# import time
+# required_Time = int(input("Enter time in seconds   >"))
+# while required_Time >0:
+#     required_Time -=1
+#     print(required_Time,"seconds")
+#     time.sleep(1)
+
+
+# HOURS,MINUTES AND SECONDS COUNTDOWN PRINT ON SAME LINE
+# import time
+# from winsound import Beep
+# hours = int(input("Enter time in hours  >"))
+# while hours > 0:
+#     hours -=1
+#     minutes = 60
+#     while minutes > 0:
+#         minutes -=1
+#         seconds = 60
+#         while seconds > 0 :
+#             seconds -=1
+#             print(f"{hours}:{minutes}:{seconds} sep="", eend = ")
+#             time.sleep(1)
+#             print("\r",end="")
+#             Beep(1000, 200)
+
+# from winsound import Beep
+# import time
+# time_allowed = int(input("please enter the alloted time  "))
+# while time_allowed > 0: 
+#     seconds = 60
+#     time_allowed -=1
+#     print(f"{time_allowed}seconds")
+#     time.sleep(1)
+#     Beep(1000,200)
+
+
+# from winsound import Beep
+# import time
+# minutes = int(input("please enter the alloted time  "))
+# while minutes > 0: 
+#     minutes =60
+#     seconds -=1
+#     print(f"{minutes}seconds")
+#     time.sleep(1)
+#     Beep(1000,200)
+
+
+# print("Generates a random number between a given positive range")
+# import random
+# r1 = int(input("insert your number  "))
+# while r1 > 0:
+#     r1 -=1
+# #     r1 = random.randint(1, 6) 
+#     print("Random number between 1 and 6 is % s" % (r1)) 
+    
+
+
+
+import random
+# r1 = int(input("insert your number  "))
+# while r1 > 0:
+#     r1 -=1
+#     r1 = random.randint(1, 6) 
+#     print(r1)
+# 
+while True :
+    input("PRESS ENTER TO DIAL")
+    die1 = random.randint(1,6)
+    die2 = random.randint(1,6)
+
+    print("DIE 1- ", die1)
+    print("DIE 2- ", die2)
+
+    if die1 == die2 == 6 :
+        print("CONGRATULATIONS YOU GOT SIKI 2...!!!")
+        break
+    else:
+        print("SORRY TRY AGAIN...")
+        
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
