082bb38c89427129137ce597b16e437759e7b76c
dtekluva <kboysreel@gmail.com>
2021-03-08 16:51:17 +0100

    assignment

--- atha/classes.py
+++ atha/classes.py
@@ -30,43 +30,73 @@
 # print(tiger)
 
 
-class Note:
+# class Note:
 
-    cover_color = "brown"
+#     cover_color = "brown"
 
-    def __init__(self, title, body, size = 10):
-        self.title = title
-        self.size = size
-        self.body = body
+#     def __init__(self, title, body, size = 10):
+#         self.title = title
+#         self.size = size
+#         self.body = body
 
-    def __str__(self) -> str:
-        return f"{self.title}. {self.size} leaves Notebook"
+#     def __str__(self):
+#         return f"{self.title}. {self.size} leaves Notebook"
 
-    def show_body(self):
-        print("Title : ", self.title.upper())
-        print("#######################")
-        print(self.body)
+#     def show_body(self):
+#         print("Title : ", self.title.upper())
+#         print("#######################")
+#         print(self.body)
 
-    def n_chars(self):
-        print(len(self.body))
+#     def n_chars(self):
+#         print(len(self.body))
+
+#     def describe(self):
+#         print("Desription. \n")
+#         print(self.title)
+#         print("Title length : ",len(self.title))
+#         print("Body length : ",len(self.body))
+#         print("Color : ",self.cover_color)
+
+
+
+# note1 = Note("happy day", "Today is a happy day")
+# note2 = Note("hehehe", "Nothing worth reading")
+# note3 = Note("ooupsie", "Mistake while typing. LOL", 30)
+
+
+# # print(note1)
+# # print(note3)
+# # print(note2)
+
+# # note3.show_body()
+# note3.n_chars()
+# note1.describe()
+# # print(note1.cover_color)
+
+
+class Employee():
+
+    company_name = "New Age Corp."
+
+    def __init__(self, name, age, designation, salary, years_of_work) -> None:
+        self.name = name
+        self.age = age
+        self.designation = designation
+        self.salary = salary
+        self.years_of_work = years_of_work
 
     def describe(self):
-        print("Desription. \n")
-        print(self.title)
-        print("Title length : ",len(self.title))
-        print("Body length : ",len(self.body))
+        print("Name : ", self.name)
+        print("Age : ", self.age)
+        print("designation : ", self.designation)
+        print("salary : ", self.salary)
+        print("Work years : ", self.years_of_work)
+        print("Yearly bonus : ", self.yearly_bonus())
 
+    def yearly_bonus(self):
+        return self.salary * self.years_of_work * 0.1
 
-note1 = Note("happy day", "Today is a happy day")
-note2 = Note("hehehe", "Nothing worth reading")
-note3 = Note("ooupsie", "Mistake while typing. LOL", 30)
+employee1 = Employee("John Williams", 41, "HR", 2304000, 2)
+employee1.describe()
 
-
-# print(note1)
-# print(note3)
-# print(note2)
-
-# note3.show_body()
-note3.n_chars()
-note1.describe()
-print(note1.cover_color)
+# print(employee1.yearly_bonus())
