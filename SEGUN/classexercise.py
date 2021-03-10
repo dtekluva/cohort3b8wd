6dd14e8337d3a1bb21a3b90d8e956eb05921abb9
Lloyds01 <soloyede708@gmail.com>
2021-03-10 23:03:04 +0800

    XC

--- SEGUN/classexercise.py
+++ SEGUN/classexercise.py
@@ -39,40 +39,69 @@
 # print(note1.cover_color)
 
 
-class employee():
+# class employee():
 
-    company_name = "shell global venture"
+#     company_name = "shell global venture"
 
-    def __init__(self, name, age,designation,salary,yrs_work):
-        self.name = name
-        self.age = age
-        self.designation= designation
-        self.salary = salary
-        self.yrs_work = yrs_work
+#     def __init__(self, name, age,designation,salary,yrs_work):
+#         self.name = name
+#         self.age = age
+#         self.designation= designation
+#         self.salary = salary
+#         self.yrs_work = yrs_work
 
+
+#     def describe(self):
+#         print("description. \n")
+#         print("name :", self.name)
+#         print("age :", self.age)
+#         print("designation :", self.designation)
+#         print("salary :", self.salary)
+#         print("yrs_work :", self.yrs_work)
+#         print("yearly bonus :", self.yearly_bonus)
+        
+#     def yearly_bonus(self):
+#         return self.salary*self.yrs_work //100
+
+# employee1 = employee("Alex Dokubo", 35, "ADMIN", 400000, 2)
+# employee2 = employee("bola adebiyi", 55, "ADMIN", 4900000, 2)
+# employee1.describe()
+# employee2.describe()
+# print(employee1.yearly_bonus() )
+# print(employee2.yearly_bonus()) 
+
+
+
+class humans:
+    skin = True
+    brain = True
+    num_leg = 2
+    num_ears = 2
+
+class european(humans):
+    skin_color= "white"
+    hair_type = "silky"
 
     def describe(self):
-        print("description. \n")
-        print("name :", self.name)
-        print("age :", self.age)
-        print("designation :", self.designation)
-        print("salary :", self.salary)
-        print("yrs_work :", self.yrs_work)
-        print("yearly bonus :", self.yearly_bonus)
+        print(f"i am a {self.skin_color}....and have {self.hair_type} hair")
         
-    def yearly_bonus(self):
-        return self.salary*self.yrs_work //100
 
-employee1 = employee("Alex Dokubo", 35, "ADMIN", 400000, 2)
-employee2 = employee("bola adebiyi", 55, "ADMIN", 4900000, 2)
-employee1.describe()
-employee2.describe()
-print(employee1.yearly_bonus() )
-print(employee2.yearly_bonus()) 
+class employee(european):
 
+    def __init__(self, name,age,salary,dept):
+        self.name = name
+        self.age = age
+        self.salary = salary
+        self.dept = dept
 
+    def describe(self):
+        super().describe()
+        print("my name is",self.name)
+        print("i am ",self.age, "yrs old")
+        print("i earn a total of",self.salary, "monthly and")
+        print("i work in",self.dept,"of telly4africa media")
 
-
-
+teanm=employee("segun", 34, 20000, "HR")
+teanm.describe()
 
     
---
+++ SEGUN/week8ass.py
@@ -0,0 +1,48 @@
+# Please create a class sortedlist that has one attribute 
+# target list and has 2 methods namely:
+
+# Merge sort
+# Bubble sort
+
+# when called i.e
+
+# class sortedlist:
+#     pass
+
+#     def __init__(self, target_list):
+#         pass
+
+# >>>my_list = [2,4,1]
+
+# >>>my_sorted_list = sortedlist(my_list)
+
+# >>>my_sorted_list.bubble_sort()
+# >>> [1,2,3]
+
+# >>>my_sorted_list.merge_sort()
+# >>> [1,2,3]
+
+# create a decorator that can makes the function also show the time taken to run
+# class sorted_list:
+#     pass
+#     def __init__()
+
+class sorted_list():
+    pass
+
+    def __init__(self, target_list):
+        self.target_list = target_list
+    pass
+        
+
+    def merger_sort(self):
+        my_list = [4,6,12,15]
+        my_sorted_list = sorted(my_list)
+        my_sorted_list.merge_sort()
+        [4,3,2,1] 
+
+    
+        my_sorted_list.bubble_sort()
+        [4,3,2,1]
+        
+sorted_list(2)
